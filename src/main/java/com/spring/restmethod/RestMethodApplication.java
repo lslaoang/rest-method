d3cfd6802b57761552adc19e4cf044e82040b2dd
Laurel Lao Ang <laurellaoang@gmail.com>
2022-04-22 09:41:32 +0800

    Initial commit

---
+++ src/main/java/com/spring/restmethod/RestMethodApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMethodApplication.class, args);
+    }
+
+}
